ef9523873f619545462d47ca68c4b1074fbcb5b2
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-07-12 09:46:17 -0500

    agregando el whatsapp

--- js/reserva.js
+++ js/reserva.js
@@ -1,13 +1,15 @@
 import moment from "moment";
 // import "moment/locale/es";
 
-if (window.location.pathname === "/indexes.html") {
+if (window.location.pathname === !"/") {
   import("moment/locale/es").then((lang) => {
     return lang;
   });
   moment.locale("es");
   console.log("es")
 }
+
+
 
 const revisar = document.getElementById("revisar");
 //inputs
@@ -48,17 +50,19 @@ const actualizar = (e) => {
 
 
   const fechaE = moment(fecha.value).format("dddd D MMMM YYYY");
- 
+
   const mensaje = `
   <hr>
-  <b>Please check that are correct the data</b>
+  <b>Please check that are correct the data / Por favor revise los datos</b>
   <hr>
-Name: <b> ${nombre.value} </b> <br>
+Name / Nombre: <b> ${nombre.value} </b> <br>
 Email: <b> ${email.value} </b> <br>
-Event date: <b> ${fechaE} </b> <br>
-Invites: <b> ${invitados.value}</b>
+Event date / Fecha del evento: <b> ${fechaE} </b> <br>
+Invites / Invitados: <b> ${invitados.value}</b>
 `;
-  revisar.innerHTML = mensaje;
+
+
+ revisar.innerHTML = mensaje;
   envio( whats, fechaE);
 };
 
@@ -66,14 +70,14 @@ Invites: <b> ${invitados.value}</b>
 
 const envio = (whats, fechaE) => {
   const url = `
-  ${whats}Hola,%20me%20contacto%20desde%20zihuacentro.com,%20deseo%20cotizar:%0aNombre:%20${nombre.value},%0aNo.%20de%20adultos:%20${email.value},%0aNo.%20de%20niños:%20${fechaE}%0aFecha%20de%20Llagada:%20${invitados.value}%0aComentarios:%20
+  ${whats}Hola,%20me%20contacto%20desde%20weddingsinzihua.com%0aNombre:%20${nombre.value},%0aEmail:%20${email.value},%0aFecha%20del%20evento:%20${fechaE}%0aNo.%20de%20invitados:%20${invitados.value}%0ahablemos de tu evento%20😁:%20
   `;
 
   const urlEn = `
-  ${whats}Hi,%20I%20contact%20from%weddingsinzihua.com.:%0aName:%20${nombre.value},%0aNo.%20de%20Email:%20${email.value}%0aEvent%20de%20date:%20${fechaE}%0aInvites:%20${invitados.value}%0aLet's%20talk%20😁:%20
+  ${whats}Hi,%20I%20contact%20from%20weddingsinzihua.com.:%0aName:%20${nombre.value}%0aEmail:%20${email.value}%0aEvent%20de%20date:%20${fechaE}%0aInvites:%20${invitados.value}%0aLet's%20talk%20😁:%20
   `;
 
-  if (window.location.pathname === "/") {
+  if (window.location.pathname === "/indexes.html") {
     btncotizar.href = url;
   } else {
     btncotizar.href = urlEn;
